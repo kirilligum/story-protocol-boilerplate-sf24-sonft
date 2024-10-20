1c3cb308031620884ad634aededc999d2a5205d5
Kirill Igumenshchev <kirill.igum@gmail.com>
2024-10-20 04:54:21 -0700

    update

--- src/SONFT.sol
+++ src/SONFT.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ONFT721 } from "@layerzerolabs/onft-evm/contracts/onft721/ONFT721.sol";
 
 contract SONFT is ONFT721 {
+    uint256 public nextTokenId;
     constructor(
         string memory _name,
         string memory _symbol,
         address _lzEndpoint,
         address _delegate
-    ) ONFT721(_name, _symbol, _lzEndpoint, _delegate)  Ownable(msg.sender) {}
+    ) ONFT721(_name, _symbol, _lzEndpoint, _delegate) {}
+
+    function mint(address to) public onlyOwner returns (uint256) {
+        uint256 tokenId = nextTokenId++;
+        _mint(to, tokenId);
+        return tokenId;
+    }
 }
